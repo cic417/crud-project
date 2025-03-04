9c5944a5f81147e78c3ab3bc770d85b15490dee1
cic417 <mz7731346@gmail.com>
2025-03-03 21:01:02 -0600

    updated db url

--- server/config/db.js
+++ server/config/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
-// const mongoURI = 'mongodb+srv://crudapp:crudapp@cluster0.iydyt0r.mongodb.net/?retryWrites=true&w=majority';
-const mongoURI = 'mongodb://localhost:27017/crud';
+const mongoURI = 'mongodb+srv://mz7731346:kGdWOtV7Wto1kkxI@cluster0.r7mtc.mongodb.net/myfirstdatabase?retryWrites=true&w=majority&appName=Cluster0';
+// const mongoURI = 'mongodb://localhost:27017/crud';
 
 const connectDB = async () => {
   try {
