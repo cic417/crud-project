d68ab1c0989705687063436f2c8b3901eb139413
cic417 <mz7731346@gmail.com>
2025-03-04 17:56:22 -0600

    updated header

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -104,7 +104,7 @@ const App = () => {
   return (
     <div>
       <div className="container">
-        <h1>MERN Stack Project (CRUD) - Hayyan shaikh</h1>
+        <h1>MERN Stack Project (CRUD)</h1>
         <form onSubmit={handleAddUser}>
           <input
             type="text"
